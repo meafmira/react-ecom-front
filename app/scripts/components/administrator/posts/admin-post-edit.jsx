749482a826dbd5aa067ad4a24c385dafa3e8b374
meafmira <gang7e@gmail.com>
2015-04-24 14:05:51 +0500

    Post edit form

--- app/scripts/components/administrator/posts/admin-post-edit.jsx
+++ app/scripts/components/administrator/posts/admin-post-edit.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PostActions from 'actions/post';
 import PostStore from 'stores/post';
+import AdminPostForm from 'components/administrator/posts/admin-post-form'
 
 export default class AdminPostEdit extends React.Component {
   constructor() {
@@ -8,8 +9,6 @@ export default class AdminPostEdit extends React.Component {
       post: {}
     }
     this.onLoadPost = this.onLoadPost.bind(this);
-    this.handleChangeText = this.handleChangeText.bind(this);
-    this.handleChangeTitle = this.handleChangeTitle.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
@@ -29,50 +28,19 @@ export default class AdminPostEdit extends React.Component {
     this.setState({post});
   }
 
-  handleChangeTitle(e) {
-    let title = e.target.value
-      , post = this.state.post;
-    post.title = title;
-    this.setState({post});
-  }
-
-  handleChangeText(e) {
-    let text = e.target.value
-      , post = this.state.post;
-    post.text = text;
-    this.setState({post});
-  }
-
-  handleSubmit(e) {
-    let post = this.state.post;
+  handleSubmit(post) {
     PostActions.save(post)
       .then(() => {
         this.context.router.transitionTo('administrator-posts-category', { categoryId: post.post_category_id });
-      })
-    e.preventDefault();
+      });
   }
 
   render() {
-    let post = this.state.post;
     return (
-      <div>
-        <h1 className="page-header">{ post.title }</h1>
-        <form onSubmit={ this.handleSubmit }>
-          <div className="form-group">
-            <label>Заголовок</label>
-            <input className="form-control" value={ post.title } onChange={ this.handleChangeTitle } />
-          </div>
-          <div className="form-group">
-            <label>Текст</label>
-            <textarea rows="10" className="form-control" value={ post.text } onChange={ this.handleChangeText }></textarea>
-          </div>
-          <div className="btn-group">
-            <button type="submit" className="btn btn-primary">Сохранить</button>
-          </div>
-        </form>
-      </div>
+      <AdminPostForm onChange={ this.handleSubmit } post={ this.state.post } />
     )
   }
+
 }
 
 AdminPostEdit.contextTypes = {
---
+++ app/scripts/components/administrator/posts/admin-post-form.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+
+export default class AdminPostForm extends React.Component {
+  constructor() {
+    this.state = {
+      post: {
+        title: "",
+        text: ""
+      }
+    }
+    this.handleChangeText = this.handleChangeText.bind(this);
+    this.handleChangeTitle = this.handleChangeTitle.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({post: this.props.post});
+  }
+
+  componentWillReceiveProps(props) {
+    this.setState({post: props.post});
+  }
+
+  handleChangeTitle(e) {
+    let title = e.target.value
+      , post = this.state.post;
+    post.title = title;
+    this.setState({post});
+  }
+
+  handleChangeText(e) {
+    let text = e.target.value
+      , post = this.state.post;
+    post.text = text;
+    this.setState({post});
+  }
+
+  handleSubmit(e) {
+    console.log(e);
+    e.preventDefault();
+    this.props.onChange(this.state.post);
+  }
+
+  render() {
+    let post = this.state.post;
+    console.log("Post: ", post);
+    return (
+      <div>
+        <h1 className="page-header">{ post.title }</h1>
+        <form onSubmit={ this.handleSubmit }>
+          <div className="form-group">
+            <label>Заголовок</label>
+            <input className="form-control" value={ post.title } onChange={ this.handleChangeTitle } />
+          </div>
+          <div className="form-group">
+            <label>Текст</label>
+            <textarea rows="10" className="form-control" value={ post.text } onChange={ this.handleChangeText }></textarea>
+          </div>
+          <div className="btn-group">
+            <button type="submit" className="btn btn-primary">Сохранить</button>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
